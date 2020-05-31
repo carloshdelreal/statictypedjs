a7ada95a7439d82c5fa34a7e3dd4628898c35a82
Carlos Del Real <carloshdelreal@gmail.com>
2020-05-31 00:28:47 -0500

    Typescript types

--- typescript.js
+++ typescript.js
@@ -1,3 +1,21 @@
 var sum = function (a, b) {
     return a + b;
 };
+console.log(sum(1, 5));
+// boolean
+var isCool = true;
+// number 
+var age = 33;
+// string
+var eyeColor = 'brown';
+var favouriteQuote = "I'm not old, I'm only " + 2;
+// Array
+var pets = ['cat', 'dog', 'pig'];
+var pets2 = ['lion', 'dragon', 'lizard'];
+// object
+var wizzard = {
+    a: 'John'
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
--- typescript.ts
+++ typescript.ts
@@ -1,4 +1,30 @@
-const sum = (a, b) => {
+const sum = (a: number, b: number) => {
   return a + b;
 }
 
+console.log(sum(1,5))
+
+
+// boolean
+let isCool: boolean = true
+
+// number 
+let age: number = 33
+
+// string
+let eyeColor: string = 'brown'
+let favouriteQuote: string = `I'm not old, I'm only ${2}`
+
+// Array
+let pets: string[] = ['cat', 'dog', 'pig']
+let pets2: Array<string> = ['lion', 'dragon', 'lizard']
+
+// object
+let wizzard: object = {
+  a: 'John'
+}
+
+// null and undefined
+
+let meh: undefined = undefined;
+let noo: null = null;
